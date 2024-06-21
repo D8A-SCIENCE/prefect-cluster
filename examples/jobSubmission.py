b989a04d42edb0555ca00ceeaf899fde8fcad914
DanRunfola <dan@danrunfola.com>
2024-06-21 16:15:54 -0400

    no resource spec yet

--- examples/jobSubmission.py
+++ examples/jobSubmission.py
@@ -8,39 +8,10 @@ testFlow = flow.from_source(
     entrypoint="examples/simpleJob.py:testFlow"
 )
 
-# Define Kubernetes Job with resource specifications
-# Define Kubernetes Job with resource specifications using the new package
-k8s_job = KubernetesJob(
-    job={
-        "metadata": {
-            "labels": {
-                "example": "kubernetes-job"
-            }
-        },
-        "spec": {
-            "template": {
-                "spec": {
-                    "containers": [{
-                        "name": "flow-container",
-                        "image": "ghcr.io/d8a-science/prefect-kubectl:latest",
-                        "resources": {
-                            "limits": {
-                                "cpu": "2",
-                                "memory": "4Gi"
-                            }
-                        }
-                    }]
-                }
-            }
-        }
-    }
-)
-
 # Create the deployment
 deployment = Deployment.build_from_flow(
     flow=testFlow,
     name="exampleDeployment",
     work_queue_name="k8s-dynamic",
-    tags=["k8s", "example"],
-    infrastructure=k8s_job
+    tags=["k8s", "example"]
 )
